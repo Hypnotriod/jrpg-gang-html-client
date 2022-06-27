c17db9b0e4ea50c85971c35091060aac0e2f94fa
Ilya Pikin <hypnotiod@gmail.com>
2022-06-27 22:21:52 +0300

    updated item description popup

--- src/component/ui/popup/ItemDescription.ts
+++ src/component/ui/popup/ItemDescription.ts
@@ -1,3 +1,4 @@
+import { Ammunition, Disposable } from '../../../domain/domain';
 import TextField from '../textfield/TextField';
 
 export default class ItemDescription extends TextField {
@@ -10,18 +11,50 @@ export default class ItemDescription extends TextField {
         });
     }
 
-    public set data(data: Object) {
-        this.value = this.mapValues(data);
+    public set data(data: Disposable | Ammunition) {
+        this.value = this.render(data, '');
     }
 
-    protected mapValues(obj: Object): string {
-        let result = '';
-        Object.keys(obj).forEach(key => result +=
-            obj[key] ?
-                (obj[key] instanceof Object ?
-                    `${key}:<br> ${this.mapValues(obj[key])}` :
-                    `${key}: ${obj[key]}<br>`) : '',
-        );
-        return result;
+    protected render(data: Object, key: string, deep: number = 0): string {
+        if (key === 'uid' || key === 'code') { return ''; }
+        if (typeof data === 'string' || typeof data === 'number' || typeof data === 'boolean') {
+            return this.valueNotZeroOrEmpty(key, data as (string | number | boolean));
+        } else if (data instanceof Array) {
+            return this.renderObjects(data, key, deep);
+        } else {
+            return this.renderObject(data, key, deep);
+        }
+    }
+
+    protected renderObjects(data: Object[], header: string, deep: number): string {
+        if (!data || !data.length) { return ''; }
+        const result = data.reduce((acc, d, i) => acc + this.renderObject(d, `${header} #${i + 1}`, deep), '');
+        return result.toString();
+    }
+
+    protected renderObject(data: Object, header: string, deep: number): string {
+        if (!data || this.emptyOrAllFieldsZeros(data)) { return ''; }
+        return this.header(header, deep) + Object.keys(data)
+            .reduce((acc, key) => acc + this.render(data[key], key, deep + 1), '');
+    }
+
+    protected header(value: string, deep: number): string {
+        switch (deep) {
+            case 0:
+                return '';
+            case 1:
+                return `<span class="light-green lighten-1">${value}:</span><br>`;
+            default:
+            case 2:
+                return `<span class="light-blue lighten-1">${value}:</span><br>`;
+        }
+    }
+
+    protected valueNotZeroOrEmpty(key: string, value: number | string | boolean | undefined): string {
+        return value ? `<span class="orange-text text-darken-1">${key}</span>: ${value}<br>` : '';
+    }
+
+    protected emptyOrAllFieldsZeros(data: Object): boolean {
+        return !data || Object.keys(data).every(key => !data[key]);
     }
 }
