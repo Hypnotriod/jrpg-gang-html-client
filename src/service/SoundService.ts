9cd5974b73467b68aa888540231770f1dc7e9bc8
illia <illia.pikin@gmail.com>
2026-03-04 11:08:42 +0200

    Use Howler.js library for sounds

--- src/service/SoundService.ts
+++ src/service/SoundService.ts
@@ -1,3 +1,5 @@
+import { Howl } from 'howler';
+
 export enum SoundName {
     BUFF = 'buff',
     CLICK = 'click',
@@ -16,42 +18,33 @@ export enum SoundName {
 }
 
 export class SoundService {
-    private static readonly private: { [Key in SoundName]?: HTMLAudioElement } = {};
-    private static ready: boolean = false;
+    private static readonly sounds: { [Key in SoundName]?: Howl } = {};
 
     public static initialize(): void {
-        SoundService.private[SoundName.BUFF] = new Audio('assets/sounds/buff.mp3');
-        SoundService.private[SoundName.CLICK] = new Audio('assets/sounds/click.mp3');
-        SoundService.private[SoundName.DEATH] = new Audio('assets/sounds/death.mp3');
-        SoundService.private[SoundName.DENIED] = new Audio('assets/sounds/denied.mp3');
-        SoundService.private[SoundName.EQUIP] = new Audio('assets/sounds/equip.mp3');
-        SoundService.private[SoundName.HIT] = new Audio('assets/sounds/hit.mp3');
-        SoundService.private[SoundName.MISS] = new Audio('assets/sounds/miss.mp3');
-        SoundService.private[SoundName.MOVE] = new Audio('assets/sounds/move.mp3');
-        SoundService.private[SoundName.TREASURE] = new Audio('assets/sounds/treasure.mp3');
-        SoundService.private[SoundName.REPAIR] = new Audio('assets/sounds/repair.mp3');
-        SoundService.private[SoundName.STUNNED] = new Audio('assets/sounds/stunned.mp3');
-        SoundService.private[SoundName.HORN] = new Audio('assets/sounds/horn.mp3');
-        SoundService.private[SoundName.DOOR] = new Audio('assets/sounds/door.mp3');
-        SoundService.private[SoundName.COMPLETE] = new Audio('assets/sounds/complete.mp3');
-
-        window.document.addEventListener('mousedown', () => (SoundService.ready = true));
+        SoundService.sounds[SoundName.BUFF] = new Howl({ src: ['assets/sounds/buff.mp3'] });
+        SoundService.sounds[SoundName.CLICK] = new Howl({ src: ['assets/sounds/click.mp3'] });
+        SoundService.sounds[SoundName.DEATH] = new Howl({ src: ['assets/sounds/death.mp3'] });
+        SoundService.sounds[SoundName.DENIED] = new Howl({ src: ['assets/sounds/denied.mp3'] });
+        SoundService.sounds[SoundName.EQUIP] = new Howl({ src: ['assets/sounds/equip.mp3'] });
+        SoundService.sounds[SoundName.HIT] = new Howl({ src: ['assets/sounds/hit.mp3'] });
+        SoundService.sounds[SoundName.MISS] = new Howl({ src: ['assets/sounds/miss.mp3'] });
+        SoundService.sounds[SoundName.MOVE] = new Howl({ src: ['assets/sounds/move.mp3'] });
+        SoundService.sounds[SoundName.TREASURE] = new Howl({ src: ['assets/sounds/treasure.mp3'] });
+        SoundService.sounds[SoundName.REPAIR] = new Howl({ src: ['assets/sounds/repair.mp3'] });
+        SoundService.sounds[SoundName.STUNNED] = new Howl({ src: ['assets/sounds/stunned.mp3'] });
+        SoundService.sounds[SoundName.HORN] = new Howl({ src: ['assets/sounds/horn.mp3'] });
+        SoundService.sounds[SoundName.DOOR] = new Howl({ src: ['assets/sounds/door.mp3'] });
+        SoundService.sounds[SoundName.COMPLETE] = new Howl({ src: ['assets/sounds/complete.mp3'] });
     }
 
-    public static play(name: SoundName, options?: { delayMs?: number }): void {
-        const sound = SoundService.private[name];
-        if (!SoundService.ready || !sound) return;
-        const onCanPlayThrough = () => {
-            sound.removeEventListener('canplaythrough', onCanPlayThrough);
-            sound.play();
+    public static play(name: SoundName, options?: { delayMs?: number, loop?: boolean; }): void {
+        const sound = SoundService.sounds[name];
+        if (!sound) return;
+        sound.loop(options?.loop ?? false);
+        if (options?.delayMs) {
+            setTimeout(() => sound.play(), options.delayMs);
+            return;
         }
-        setTimeout(() => {
-            sound.currentTime = 0;
-            if (sound.readyState === HTMLMediaElement.HAVE_ENOUGH_DATA) {
-                sound.play();
-            } else {
-                sound.addEventListener('canplaythrough', onCanPlayThrough, false);
-            }
-        }, options?.delayMs ?? 0);
+        sound.play();
     }
 }
